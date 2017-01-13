a4569674b8c0b9f3213e0eb6135133b2ba871a79
Pikarelia <ana.grigorcea@yahoo.com>
2017-01-13 08:11:51 +0200

    I did it, puzzle game

--- src/Main.java
+++ src/Main.java
@@ -16,11 +16,14 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Ramocika");
-        //frame.setMinimumSize(new Dimension(500, 700));
+        frame.setMinimumSize(new Dimension(600, 315));
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setResizable(false);
-        frame.setContentPane(new MeinPanel());
+        frame.setContentPane(new StartPanel());
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 }
